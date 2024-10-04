e8088ce5390a06aaef945b32fac1ced8f3e12c99
mustadj <mustadjamilah@gmail.com>
2024-10-04 20:33:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,6 @@ def countdown_timer(duration):
         sys.stdout.flush()
         time.sleep(1)
         duration -= 1
-    sys.stdout.write("\rCountdown selesai. Melanjutkan proses...\n")  # Kursor pindah ke bawah setelah selesai
     sys.stdout.flush()
 
 # Fungsi untuk menginisialisasi session requests dengan logika retry
@@ -156,8 +155,8 @@ def paint(canvas_pos, color, header):
     try:
         response = session.post(f"{url}/repaint/start", data=json.dumps(data), headers=header, timeout=10)
         if response.status_code == 400:
-            log_message("Painter: No charge available. Sleeping for 60 minutes.", Fore.BLUE)
-            countdown_timer(60 * 60)
+            log_message("Painter: No charge available. Sleeping for 10 minutes.", Fore.BLUE)
+            countdown_timer(10 * 60)
             log_message("Timer selesai. Melanjutkan eksekusi.", Fore.YELLOW)
             return False
         if response.status_code == 401:
@@ -260,4 +259,4 @@ akun_list = load_accounts_from_file("data.txt")
 if akun_list:
     main(akun_list[0], akun_list[0])
 else:
-    log_message("Tidak ada akun yang ditemukan di data.txt", Fore.RED)
+    log_message("Tidak ada akun yang ditemukan di data.txt", Fore.RED)  
